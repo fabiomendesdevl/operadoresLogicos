92f4224d4e196ff8cf38bb90c6bd90110325863c
Fabio Mendes <fabiomendesdev@outlook.com>
2023-11-23 16:25:08 -0300

    operadores Logicos

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+
+{
+    Produto mouse = new Produto(1, "mouse gamer", 299.97);
+    Console.WriteLine(mouse.Id);
+    Console.WriteLine(mouse.Nome);
+    Console.WriteLine(mouse.Price);
+}
+struct Produto
+{
+    public Produto(int id, string nome, double price)
+    {
+        Id = id;
+        Nome = nome;
+        Price = price;
+    }
+    public int Id;
+    public string Nome;
+    public double Price;
+
+    public double PriceInDolar(double dolar)
+    {
+        return Price * dolar;
+    }
+}
